66af0736842d29ef5087a1913810e9256c7e4ec6
Carlos Augusto Monoo Pereira Barbosa <augustomonoo@gmail.com>
2025-10-06 19:15:58 -0300

    Added utilities to handle django csrf

---
+++ htmlelements/django/csrf.py
@@ -0,0 +1,20 @@
+from django.middleware.csrf import get_token
+from htmlelements.dynamic import Input
+from django.http import HttpRequest
+
+
+def csrf_token(request: HttpRequest):
+    """Wrapper for django's get_token.
+
+    Returns a callable so that the token can be obtained only when necessary
+    """
+
+    def csrf():
+        return get_token(request)
+
+    return csrf()
+
+
+def csrf_input(request: HttpRequest, type="hidden", name="csrfmiddlewaretoken"):
+    """A Input[type='hidden'] element with django's csrf_token"""
+    return Input(value=csrf_token(request), type=type, name=name)
